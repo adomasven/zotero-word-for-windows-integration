0857372250f17834c59279f19fd38306e3e306dd
Adomas Venčkauskas <adomas.ven@gmail.com>
2017-12-01 12:25:06 +0200

    Use 3.5.6 dlls

--- components/zoteroWinWordIntegration.js
+++ components/zoteroWinWordIntegration.js
@@ -135,9 +135,6 @@ function init() {
 		// statusCode cleanup(Document *doc);
 		"cleanup":lib.declare("cleanup", ctypes.stdcall_abi, statusCode, document_t.ptr),
 
-		// statusCode complete(Document *doc);
-		"complete":lib.declare("complete", ctypes.stdcall_abi, statusCode, document_t.ptr),
-		
 		// statusCode deleteField(Field* field);
 		"deleteField":lib.declare("deleteField", ctypes.stdcall_abi, statusCode, field_t.ptr),
 			
@@ -330,7 +327,6 @@ Document.prototype = {
 	"complete":function() {
 		Zotero.debug("ZoteroWinWordIntegration: complete", 4);
 		if(this._documentStatus.active) {
-			checkStatus(f.complete(this._document_t));
 			f.freeDocument(this._document_t);
 			this._documentStatus.active = false;
 		} else {
